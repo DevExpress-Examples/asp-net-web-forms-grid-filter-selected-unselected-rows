08876a8e0f4447ccda04be7914c53cc309216ee5
DevExpressExampleBot <fakeEmail>
2022-12-07 14:59:27 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using DevExpress.Data.Filtering;
-using DevExpress.Web.ASPxGridView;
+using DevExpress.Web;
 
 public partial class _Default : System.Web.UI.Page {
     protected void grid_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -2,7 +2,7 @@ Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports DevExpress.Data.Filtering
-Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web
 
 Partial Public Class _Default
 	Inherits System.Web.UI.Page
